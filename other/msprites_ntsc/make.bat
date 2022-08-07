8e63d2f53d28a89b308685e824e3d87f4b4f2bd1
sunlitspace542 <rabbidsluis123@gmail.com>
2022-08-07 17:01:50 -0400

    add files for building msprites

---
+++ other/msprites_ntsc/make.bat
@@ -0,0 +1,7 @@
+del *.bin
+merge.exe SPT_0.CGX SPT_1.CGX SPT_01.BIN /cgx
+merge.exe SPT_2.CGX SPT_3.CGX SPT_23.BIN /cgx
+merge.exe SPT_4.CGX SPT_5.CGX SPT_45.BIN /cgx
+merge.exe SPT_2.CGX SPT_3_A.CGX SPT_23A.BIN /cgx
+merge.exe SPT_4_A.CGX SPT_5_A.CGX SPT_45A.BIN /cgx
+exit
