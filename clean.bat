e160afed4e7d3d7d9cd9443e44fc413a68a910de
sunlitspace542 <sunlitspace542@gmail.com>
2023-12-26 20:53:24 -0500

    actual make clean fix

--- clean.bat
+++ clean.bat
@@ -1,19 +1,10 @@
 cls
 set path=%path%;c:\bin
-set sasmheap=14400
-cd sf
+cd sf2
 @echo off
 echo Cleaning...
 make clean
 cd ..
-del SF2\BANK\*.SOB
-del SF2\MSPRITES\*.BIN
-del SF2\DATA\COL\ALLCOLS.COL
-del SF2\DATA\COL\*.PAC
-del SF2\DATA\COL\*.TMP
-del SF2\DATA\*.CCR
-del SF2\DATA\*.PCR
-del SF2\SF2.SFC
 del sf2.SFC
 del symbols.txt
 exit
