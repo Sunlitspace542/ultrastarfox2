d32bb3be069041b878e2236508dbce055c55e78c
sunlitspace542 <sunlitspace542@gmail.com>
2023-12-26 20:51:14 -0500

    make clean hotfix

--- clean.bat
+++ clean.bat
@@ -6,6 +6,14 @@ cd sf
 echo Cleaning...
 make clean
 cd ..
+del SF2\BANK\*.SOB
+del SF2\MSPRITES\*.BIN
+del SF2\DATA\COL\ALLCOLS.COL
+del SF2\DATA\COL\*.PAC
+del SF2\DATA\COL\*.TMP
+del SF2\DATA\*.CCR
+del SF2\DATA\*.PCR
+del SF2\SF2.SFC
 del sf2.SFC
 del symbols.txt
 exit
